d2c23ffb09d33a2c9477c57c2225cead0bba7c5a
Jeff <hush2dev@gmail.com>
2019-04-12 08:41:57 +0800

    Extract error icon

---
+++ lib/widgets/error_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart' show Widget, Icon, Icons, Colors;
+
+Widget errorIcon() {
+  return Icon(
+    Icons.warning,
+    size: 30,
+    color: Colors.yellow[700],
+  );
+}
